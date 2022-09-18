f5ad723b7e443cdb33d61f3a19d69427627cb8ce
Ahmed Elneziliiy <ahmedgamalgamal123456@gmail.com>
2022-09-18 15:16:08 +0200

    Add CORS - ConnectionString

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,13 +1,20 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using MoviesAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+ 
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+    options.UseSqlServer(builder.Configuration.GetConnectionString(name: "DefaultConnection"))
+);
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
+
 builder.Services.AddSwaggerGen(option =>
 {
     option.SwaggerDoc(name: "v1", info: new OpenApiInfo
@@ -66,9 +73,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(c=>c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
